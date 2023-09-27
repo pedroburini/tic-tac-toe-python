cbeb44b734fe825758d2b97a5e4b3cecc010f286
Pedro Burini <pulisses10@gmail.com>
2023-09-27 13:37:57 -0300

    asks for a player's next position and then uses the function space_check to check if its a free position. If it is, then return the position for later use

---
+++ player_choice.py
@@ -0,0 +1,9 @@
+from space_check import space_check
+
+def player_choice(board):
+    position = 0
+    
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+        position = int(input('Choose your next position: (1-9) '))
+        
+    return position
