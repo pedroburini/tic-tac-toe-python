44fdffd4bbc7b729c199487aa01bb5de7c97db1d
Pedro Burini <pulisses10@gmail.com>
2023-09-27 13:13:32 -0300

    function that prints out the board

---
+++ display_board.py
@@ -0,0 +1,17 @@
+from IPython.display import clear_output
+
+def display_board(board):
+    clear_output()  # Remember, this only works in jupyter!
+    
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+    
