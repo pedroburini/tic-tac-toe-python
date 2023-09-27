f2bd072688c95643673519ba8b03aed02e1f4a1a
Pedro Burini <pulisses10@gmail.com>
2023-09-27 13:15:37 -0300

    take in a player input and assign their marker as 'X' or 'O'

---
+++ player_input.py
@@ -0,0 +1,11 @@
+def player_input():
+    marker = ''
+    
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O? ').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+    
